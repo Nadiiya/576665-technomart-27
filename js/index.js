1b9b3ebb92bc2d0bd7f32cb831826e67553f9c62
Nadiiy <nadiya.derevyanchenko@gmail.com>
2020-03-04 17:46:03 +0200

    add modal window on catalog-page

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
 const link = document.querySelector(".contacts-button");
-
 const popup = document.querySelector(".modal-back");
 const close = document.querySelector(".modal-close");
 
 const form = document.querySelector(".sender-form");
 const senderName = form.querySelector("[name=name]");
-const senderEmail = form.querySelector("[name=email]");
-const message = form.querySelector("[name=message]");
 
-link.addEventListener('click', function (evt) {
-evt.preventDefault();
-popup.classList.add("modal-show");
-senderName.focus();
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  senderName.focus();
 });
+
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -20,35 +19,29 @@ close.addEventListener("click", function (evt) {
   popup.classList.remove("modal-error");
 });
 
-form.addEventListener("submit", function (evt) {
-  if (!senderName.value || !senderEmail.value || !message.value) {
-    evt.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
-  }
-});
 
 const mapImage = document.querySelector(".map");
 const mapPopup = document.querySelector(".modal-map");
 const mapClose = mapPopup.querySelector(".modal-close");
+
 
 mapImage.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.add("modal-show");
 });
 
+
 mapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.remove("modal-show");
 });
+
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
-      popup.classList.remove("modal-error");
     }
     if (mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.remove("modal-show");
